dc42af8c34d1ab74a242f001ea1868d8fff313c7
Daniel Medeiros <danmedeirosws@gmail.com>
2022-01-14 20:50:13 -0300

    Adding initial files
    An exercise made during a class of Java on Curso Em Vídeo.

---
+++ src/br/com/guanabara/IdiomaDoSistema.java
@@ -0,0 +1,12 @@
+package br.com.guanabara;
+
+import java.util.Locale;
+
+public class IdiomaDoSistema {
+
+	public static void main(String[] args) {
+		Locale loc = Locale.getDefault();
+		System.out.println("O idioma do sistema é: ");
+		System.out.println(loc.getDisplayLanguage());
+	}
+}
---
+++ src/br/com/guanabara/ResolucaoDoSistema.java
@@ -0,0 +1,14 @@
+package br.com.guanabara;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+public class ResolucaoDoSistema {
+
+	public static void main(String[] args) {
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		Dimension d = tk.getScreenSize();
+		System.out.println("A resolução do seu sistema é: ");
+		System.out.println(d.width + "X" + d.height);
+	}
+}
